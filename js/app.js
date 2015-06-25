a6cc332172d88914ebe722ce3e84f131c5f125a7
pmaccart <phil.maccart@slalom.com>
2015-06-24 23:14:14 -0500

    Add state

--- js/app.js
+++ js/app.js
@@ -3,10 +3,17 @@
 var React = require('react');
 
 var App = React.createClass({
-  
+
+  getInitialState: function() {
+    return {
+      count: 1
+    }
+  },
+
   render: function() {
     return <div>
       <h2>Hello {this.props.name}!</h2>
+      <h4>Count: {this.state.count}</h4>
     </div>;
   }
 });
