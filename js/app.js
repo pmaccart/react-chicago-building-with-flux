914e185fb973070f7e96b721852bc79487121488
pmaccart <phil.maccart@slalom.com>
2015-06-25 01:29:59 -0500

    Inputs

--- js/app.js
+++ js/app.js
@@ -6,7 +6,8 @@ var App = React.createClass({
 
   getInitialState: function() {
     return {
-      count: 1
+      count: 1,
+      controlledValue: ''
     }
   },
 
@@ -15,7 +16,30 @@ var App = React.createClass({
       <h2>Hello {this.props.name}!</h2>
       <h4>Count: {this.state.count}</h4>
       <button type="button" className="btn btn-primary" onClick={this._incrementCount}>Increment Count</button>
+
+      <form>
+        <div className="form-group">
+          <label for="uncontrolledInput">Uncontrolled Input</label>
+          <input type="text" className="form-control" id="uncontrolledInput" />
+        </div>
+        <div className="form-group">
+          <label for="controlled">Controlled Input 1</label>
+          <input type="text" className="form-control" id="controlled" value="Hello!"/>
+        </div>
+        <div className="form-group">
+          <label for="controlled">Controlled Input 2</label>
+          <input type="text" className="form-control" id="controlled" value={this.state.controlledValue} onChange={this._updateValue}/>
+        </div>
+        <p>Uncontrolled value: {this.state.uncontrolledValue}</p>
+        <p>Controlled value: {this.state.controlledValue}</p>
+      </form>
     </div>;
+  },
+
+  _updateValue: function(e) {
+    this.setState({
+      controlledValue: e.target.value
+    });
   },
 
   _incrementCount: function() {
