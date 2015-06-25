80141667d58aa7dbd6ab447dfa7a41ff1d14488a
pmaccart <phil.maccart@slalom.com>
2015-06-25 02:07:26 -0500

    Child components

--- js/app.js
+++ js/app.js
@@ -7,7 +7,8 @@ var App = React.createClass({
   getInitialState: function() {
     return {
       count: 1,
-      controlledValue: ''
+      item: '',
+      items: []
     }
   },
 
@@ -17,28 +18,31 @@ var App = React.createClass({
       <h4>Count: {this.state.count}</h4>
       <button type="button" className="btn btn-primary" onClick={this._incrementCount}>Increment Count</button>
 
-      <form>
+      <form onSubmit={this._submitForm}>
         <div className="form-group">
-          <label for="uncontrolledInput">Uncontrolled Input</label>
-          <input type="text" className="form-control" id="uncontrolledInput" />
+          <label htmlFor="item">Item</label>
+          <input type="text" className="form-control" id="item" value={this.state.item} onChange={this._onItemChange}/>
         </div>
-        <div className="form-group">
-          <label for="controlled">Controlled Input 1</label>
-          <input type="text" className="form-control" id="controlled" value="Hello!"/>
-        </div>
-        <div className="form-group">
-          <label for="controlled">Controlled Input 2</label>
-          <input type="text" className="form-control" id="controlled" value={this.state.controlledValue} onChange={this._updateValue}/>
-        </div>
-        <p>Uncontrolled value: {this.state.uncontrolledValue}</p>
-        <p>Controlled value: {this.state.controlledValue}</p>
+        <button type="submit" className="btn btn-secondary" disabled={this.state.item.length === 0}>Add Item</button>
       </form>
+      <div>
+        <ItemsTable items={this.state.items} appDeleteHandler={this._deleteItem}/>
+      </div>
     </div>;
   },
 
-  _updateValue: function(e) {
+  _submitForm:function(e) {
+    e.preventDefault();
     this.setState({
-      controlledValue: e.target.value
+      items: this.state.items.concat(this.state.item),
+      item: ''
+    });
+
+  },
+
+  _onItemChange: function(e) {
+    this.setState({
+      item: e.target.value
     });
   },
 
@@ -46,6 +50,62 @@ var App = React.createClass({
     this.setState({
       count: this.state.count + 1
     });
+  },
+
+  _deleteItem: function(item) {
+    this.setState({
+      items: this.state.items.filter(function(i) {
+        return i !== item;
+      })
+    });
+  }
+});
+
+var ItemsTable = React.createClass({
+  getDefaultProps: function() {
+    return {
+      items: []
+    }
+  },
+
+  render: function() {
+    var rows = this.props.items.map(function(item, index) {
+      return <ItemRow key={index} item={item} tableDeleteHandler={this._deleteRow}/>
+    }.bind(this));
+
+    return <div>
+      <h4>Item count: {this.props.items.length}</h4>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rows}
+        </tbody>
+      </table>
+    </div>;
+  },
+
+  _deleteRow: function(item) {
+    this.props.appDeleteHandler(item);
+  }
+});
+
+var ItemRow = React.createClass({
+  
+  render: function() {
+    return <tr>
+      <td>{this.props.item}</td>
+      <td>
+        <button type="button" className="btn btn-link" onClick={this._onDeleteClick}>Delete</button>
+      </td>
+    </tr>;
+  },
+
+  _onDeleteClick: function() {
+    this.props.tableDeleteHandler(this.props.item);
   }
 });
 
